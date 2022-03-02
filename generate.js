8313fd1ce4496ce9a033e59df1a3d1c07adcb776
Enver Yakubov <enveryakubov@gmail.com>
2022-03-02 17:07:15 +0300

    commented generate.js

--- generate.js
+++ generate.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const { generateTemplateFiles } = require('generate-template-files');
 const { execSync } = require("child_process")
 
 const runCommand = command => {
@@ -14,35 +15,33 @@ const runCommand = command => {
 
 const gitCloneTemplate = `git clone https://github.com/Enveryakubov/templates.git`
 
-const { generateTemplateFiles } = require('generate-template-files');
-
 runCommand(gitCloneTemplate)
 
-generateTemplateFiles([
-    {
-        option: 'Create microfrontend fragment',
-        defaultCase: '(snakeCase)',
-        entry: {
-            folderPath: `./templates1/ofr-mf-boilerplate`,
-        },
-        stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
-        output: {
-            path: './Fragment',
-            pathAndFileNameDefaultCase: '(snakeCase)',
-            overwrite: true,
-        },
-    },
-    {
-        option: 'Create microfrontend host',
-        defaultCase: '(snakeCase)',
-        entry: {
-            folderPath: './templates1/ofr-mf-host',
-        },
-        stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
-        output: {
-            path: './Host',
-            pathAndFileNameDefaultCase: '(snakeCase)',
-            overwrite: true,
-        },
-    },
-]);
+// generateTemplateFiles([
+//     {
+//         option: 'Create microfrontend fragment',
+//         defaultCase: '(snakeCase)',
+//         entry: {
+//             folderPath: `./templates1/ofr-mf-boilerplate`,
+//         },
+//         stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
+//         output: {
+//             path: './Fragment',
+//             pathAndFileNameDefaultCase: '(snakeCase)',
+//             overwrite: true,
+//         },
+//     },
+//     {
+//         option: 'Create microfrontend host',
+//         defaultCase: '(snakeCase)',
+//         entry: {
+//             folderPath: './templates1/ofr-mf-host',
+//         },
+//         stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
+//         output: {
+//             path: './Host',
+//             pathAndFileNameDefaultCase: '(snakeCase)',
+//             overwrite: true,
+//         },
+//     },
+// ]);
