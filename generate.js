4deb9c27b210a145ddd81b778d27cd92b3df9ff2
Enver Yakubov <enveryakubov@gmail.com>
2022-03-02 09:42:47 +0300

    first commit

---
+++ generate.js
@@ -0,0 +1,31 @@
+const { generateTemplateFiles } = require('generate-template-files');
+
+
+generateTemplateFiles([
+    {
+        option: 'Create microfrontend fragment',
+        defaultCase: '(snakeCase)',
+        entry: {
+            folderPath: './templates/ofr-mf-boilerplate',
+        },
+        stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
+        output: {
+            path: './Fragment',
+            pathAndFileNameDefaultCase: '(snakeCase)',
+            overwrite: true,
+        },
+    },
+    {
+        option: 'Create microfrontend host',
+        defaultCase: '(snakeCase)',
+        entry: {
+            folderPath: './templates/ofr-mf-host',
+        },
+        stringReplacers: [{ question: 'Insert microfrontend name', slot: '__name__' }],
+        output: {
+            path: './Host',
+            pathAndFileNameDefaultCase: '(snakeCase)',
+            overwrite: true,
+        },
+    },
+]);
